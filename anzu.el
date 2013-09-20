e39deb4aecc7c5843459ceb1323884ffb38518df
Syohei YOSHIDA <syohex@gmail.com>
2013-09-20 23:34:46 +0900

    Add comment section

--- anzu.el
+++ anzu.el
@@ -21,6 +21,17 @@
 
 ;;; Commentary:
 
+;; `anzu.el' is Port of `anzu.vim'.
+;;
+;; `anzu.el' provides minor-mode which display 'current-posion/total matches'
+;; to mode-line in various search modes. You can understand that how many
+;; does your searched word match in current buffer.
+
+;; To use this package, add following code to your init.el or .emacs
+;;   (require 'anzu)
+;;   (global-anzu-mode t)
+;;
+
 ;;; Code:
 
 (eval-when-compile
