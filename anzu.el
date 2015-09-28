8cd99cfe7fe1e8ea3a2244a280685c575aa13328
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2015-09-29 00:07:11 +0900

    Fix invalid regexp in migemo
    When `anzu-use-migemo` is non-nil, Type "C-s [" are thrown error with
    invalid regexp.
    
    (migemo-forward "\\[" nil t)     ;* invalid-regexp
    (re-search-forward "\\[" nil t)  ;=> nil

--- anzu.el
+++ anzu.el
@@ -207,7 +207,10 @@
                                'migemo-forward
                              're-search-forward))
               (case-fold-search (anzu--case-fold-search input)))
-          (while (and (not finish) (funcall search-func input nil t))
+          (while (and (not finish)
+                      (condition-case nil
+                          (funcall search-func input nil t)
+                        (invalid-regexp nil)))
             (push (cons (match-beginning 0) (match-end 0)) positions)
             (cl-incf count)
             (when (= (match-beginning 0) (match-end 0)) ;; Case of anchor such as "^"
