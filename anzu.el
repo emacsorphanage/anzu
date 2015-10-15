d6ffd23cb5d5462afea7eecd399ed0cd9bb77838
akicho8 <akicho8@gmail.com>
2015-10-15 17:49:31 +0900

    Fix word replace in region

--- anzu.el
+++ anzu.el
@@ -661,9 +661,9 @@
       (point-max))))
 
 (defun anzu--region-begin (use-region thing backward)
-  (cond (current-prefix-arg (line-beginning-position))
+  (cond (use-region (region-beginning))
+        (current-prefix-arg (line-beginning-position))
         (thing (anzu--thing-begin thing))
-        (use-region (region-beginning))
         (backward (point-min))
         (t (point))))
 
@@ -673,10 +673,10 @@
     (line-end-position)))
 
 (defun anzu--region-end (use-region thing)
-  (cond (current-prefix-arg
+  (cond (use-region (region-end))
+        (current-prefix-arg
          (anzu--line-end-position (prefix-numeric-value current-prefix-arg)))
         (thing (anzu--thing-end thing))
-        (use-region (region-end))
         (t (point-max))))
 
 (defun anzu--begin-thing (at-cursor thing)
