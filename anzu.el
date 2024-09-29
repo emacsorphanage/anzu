bc3a0032bb6aa7f5886f10460cd53eb7b8b020af
StrawberryTea <look@strawberrytea.xyz>
2024-09-28 21:01:53 -0500

    Bump version to 0.66 in anzu.el

--- anzu.el
+++ anzu.el
@@ -5,7 +5,7 @@
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; Maintainer: LemonBreezes <look@strawberrytea.xyz>
 ;; Homepage: https://github.com/emacsorphanage/anzu
-;; Version: 0.65
+;; Version: 0.66
 ;; Package-Requires: ((emacs "25.1"))
 
 ;; This file is part of GNU Emacs.
